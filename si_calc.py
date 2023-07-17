5ec39a8a3c6dfd50ad949098135846d983572530
Najira <135957417+Najira06@users.noreply.github.com>
2023-07-17 13:42:33 +0000

    Add files via upload

---
+++ si_calc.py
@@ -0,0 +1,20 @@
+def simple_interest(principal,time,rate):
+    print(principal *rate *time)/100
+
+def principal(interest, rate,time):
+    print((100 * interest) / (rate* time))
+
+def time(interest,principal,rate):
+    print((100 * interest) / (principal * rate))
+
+def rate(interest,principal,time):
+    print((100 * interest) / (principal * time))
+
+
+
+
+
+
+
+    
+    
